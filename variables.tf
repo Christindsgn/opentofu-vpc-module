5c39d1b277283f15b55511544ed077e665460b46
Christindsgn <christinsibi333@gmail.com>
2025-01-29 16:39:37 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_name" {
+  description = "Name tag for the VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "environment" {
+  description = "Environment tag"
+  type        = string
+}
